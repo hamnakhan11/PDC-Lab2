0f907b03e450b1664433f90c21d101171f5a04c4
Hamna sadiq khan <hamnas064@gmail.com>
2025-09-23 20:59:17 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name:");
+        string username = Console.ReadLine();
+        for(int i=5; i>=0;i--)
+        {
+            Console.WriteLine(i);
+                if (i == 5) ;
+
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine($"Welcome{username}");
+    }
+
+}
